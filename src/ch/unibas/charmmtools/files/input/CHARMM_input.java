8ede60b95f2e2d37e4f878ae9a124a903acd7039
FHedin <hedin.florent@gmail.com>
2014-10-07 12:14:25 +0200

    storing enum in a new NBONDS.java file

--- src/ch/unibas/charmmtools/files/input/CHARMM_input.java
+++ src/ch/unibas/charmmtools/files/input/CHARMM_input.java
@@ -8,79 +8,6 @@
  */
 package ch.unibas.charmmtools.files.input;
 
-enum add_elec {
-
-    ELEC("ELEC"),
-    NOELectrostatics("NOEL"),
-    VDW("VDW"),
-    NOVDwaals("NOVD");
-
-    private final String keyword;
-
-    add_elec(String key) {
-        this.keyword = key;
-    }
-
-    public String getKey() {
-        return this.keyword;
-    }
-};
-
-enum add_vdw {
-
-    VDW("VDW"),
-    NOVDwaals("NOVD");
-
-    private final String keyword;
-
-    add_vdw(String key) {
-        this.keyword = key;
-    }
-
-    public String getKey() {
-        return this.keyword;
-    }
-};
-
-//enum add_ewald {
-//    EWALD("EWAL"),
-//    NOEWald("NOEW");
-//
-//    private final String keyword;
-//
-//    add_ewald(String key) {
-//        this.keyword = key;
-//    }
-//
-//    public String getKey() {
-//        return this.keyword;
-//    }
-//};
-
-enum add_elec_opt {
-
-    CDIE, // Constant dielectric. Energy is proportional to 1/R.
-    RDIE  // Distance dielectric. Energy is proportional to 1/(R-squared)
-};
-
-enum cut_type{
-
-    SHIFted("SHIF"),
-    SWITched("SWIT"),
-    FSWItched("FSWI"),
-    FSHIfted("FSHI");
-
-    private final String keyword;
-
-    cut_type(String key) {
-        this.keyword = key;
-    }
-
-    public String getKey() {
-        return this.keyword;
-    }
-};
-
 /**
  *
  * @author hedin
@@ -104,29 +31,7 @@ public abstract class CHARMM_input {
     protected String coord_String;
     //the fortran unit to use for I/O
     protected int coord_unit;
-    
-    //non bonded parameters line
-    protected String NB_params;
-    /*
-     * nbxmod : for building nonbonded list
-     * can be modified for adding more that 1,4 interactions
-     */
-    protected int nbxmod;
-    /*
-     * scalars for storing cutoff and cuton values
-     * cut_nb : Distance cutoff in generating the list of pairs
 
-     * cutoff_nb : Distance cut at which the switching function eliminates
-     * all contributions from a pair in calculating energies.
+    protected NBONDS nbonds_params;
 
-     * cuton_nb : Distance cut at which the smoothing function begins to reduce
-     * a pair's contribution. This value is not used with SHFT.
-     */
-    protected double cut_nb,cutoff_nb,cuton_nb;
-    //a scaling factor for the no-bonded 1,4 interactions
-    protected double epsilon14scalingFactor;
-    
-    
-    
-    
 }//end of class
---
+++ src/ch/unibas/charmmtools/files/input/NBONDS.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright (c) 2014, Florent Hedin, Markus Meuwly, and the University of Basel
+ * All rights reserved.
+ *
+ * The 3-clause BSD license is applied to this software.
+ * see LICENSE.txt
+ *
+ */
+package ch.unibas.charmmtools.files.input;
+
+/*
+ * Choose if nbonds calculation are Atom or Group based
+ */
+enum nbonds_type {
+
+    ATOM("ATOM"),
+    GROUp("GROU");
+
+    private final String keyword;
+
+    nbonds_type(String key) {
+        this.keyword = key;
+    }
+
+    public String getKey() {
+        return this.keyword;
+    }
+};
+
+/*
+ * Enable or disable electrostatics
+ */
+enum add_elec {
+
+    ELEC("ELEC"),
+    NOELectrostatics("NOEL");
+
+    private final String keyword;
+
+    add_elec(String key) {
+        this.keyword = key;
+    }
+
+    public String getKey() {
+        return this.keyword;
+    }
+};
+
+/*
+ * Enable or disable Van der Waals parameters
+ */
+enum add_vdw {
+
+    VDW("VDW"),
+    NOVDwaals("NOVD");
+
+    private final String keyword;
+
+    add_vdw(String key) {
+        this.keyword = key;
+    }
+
+    public String getKey() {
+        return this.keyword;
+    }
+};
+
+/*
+ * For using Ewald summation instead of standard electrostatics
+ */
+enum add_ewald {
+
+    EWALD("EWAL"),
+    NOEWald("NOEW");
+
+    private final String keyword;
+
+    add_ewald(String key) {
+        this.keyword = key;
+    }
+
+    public String getKey() {
+        return this.keyword;
+    }
+};
+
+/*
+ * CDIE is a constant dielectric term. Energy is proportional to 1/R.
+ * RDIE is a distance dependant term. Energy is proportional to 1/(R-squared)
+ */
+enum add_elec_opt {
+
+    CDIElec("CDIE"),
+    RDIElec("RDIE");
+
+    private final String keyword;
+
+    add_elec_opt(String key) {
+        this.keyword = key;
+    }
+
+    public String getKey() {
+        return this.keyword;
+    }
+};
+
+/*
+ * Choose the cutoff type
+ * SWIT - Switching function used from CTONNB to CTOFNB values.
+ * SHIF - Shifted potential acting to CTOFNB and zero beyond.
+ * FSWI - Switching function acting on force only.  Energy is integral of force.
+ * FSHI - Classical force shift method for CDIE (force has a constant offset)
+ */
+enum cut_type {
+
+    SHIFted("SHIF"),
+    SWITched("SWIT"),
+    FSWItched("FSWI"),
+    FSHIfted("FSHI");
+
+    private final String keyword;
+
+    cut_type(String key) {
+        this.keyword = key;
+    }
+
+    public String getKey() {
+        return this.keyword;
+    }
+};
+
+/*
+ * Exclusion Lists
+ *
+ * By default, vdw and electrostatic interactions between two bonded
+ * 1-2 interactions) and two atoms bonded to a common atom (1-3 interactions)
+ * atoms are excluded from the calculation of energy and forces.  Also,
+ * special vdw parameters and an electrostatic scale factor (E14FAC) can be
+ * applied to atom pairs separated by 3 bonds (1-4 interactions).  The
+ * control of the exclusion list is by the integer variable, NBXMod.
+ *
+ * NBXMod =     0        Preserve the existing exclusion lists
+ * NBXMod = +/- 1        Add nothing to the exclusion list
+ * NBXMod = +/- 2        Add only 1-2 (bond) interactions
+ * NBXMod = +/- 3        Add 1-2 and 1-3 (bond and angle)
+ * NBXMod = +/- 4        Add 1-2 1-3 and 1-4 (scaled)
+ * NBXMod = +/- 5        Add 1-2 1-3 and special 1-4 interactions
+ */
+enum nbxmod_type {
+    PRESERVE(0, "Preserve the existing exclusion lists"),
+    ADD_NOTHING(1, "Add nothing to the exclusion list"),
+    ADD_1_2(2, "Add only 1-2 (bond) interactions"),
+    ADD_1_2_AND_1_3(3, "Add 1-2 and 1-3 (bond and angle)"),
+    ADD_1_2_AND_1_3_AND_1_4_SCALED(4, "Add 1-2 1-3 and 1-4 (scaled)"),
+    ADD_1_2_AND_1_3_AND_1_4(5, "Add 1-2 1-3 and special 1-4 interactions");
+
+    private final int value;
+    private final String description;
+
+    nbxmod_type(int val, String descr) {
+        this.value = val;
+        this.description = descr;
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
+
+/**
+ *
+ * @author hedin
+ */
+public class NBONDS {
+
+    //non bonded parameters line
+    protected String NB_params;
+
+    /*
+     * nbxmod : for building nonbonded list
+     * can be modified for adding more that 1,4 interactions
+     */
+    protected int nbxmod;
+
+    /*
+     * scalars for storing cutoff and cuton values
+     * cut_nb : Distance cutoff in generating the list of pairs
+
+     * cutoff_nb : Distance cut at which the switching function eliminates
+     * all contributions from a pair in calculating energies.
+
+     * cuton_nb : Distance cut at which the smoothing function begins to reduce
+     * a pair's contribution. This value is not used with SHFT.
+     */
+    protected double cut_nb, cutoff_nb, cuton_nb;
+    //a scaling factor for the no-bonded 1,4 interactions
+    protected double epsilon14scalingFactor;
+
+    public NBONDS() {
+
+    }
+
+}
