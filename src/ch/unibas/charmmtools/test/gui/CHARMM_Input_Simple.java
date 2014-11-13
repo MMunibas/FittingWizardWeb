962336a78f51de4b7098bc9e63a6b459848718e3
FHedin <hedin.florent@gmail.com>
2014-11-13 17:29:55 +0100

    some fields added to interface

--- src/ch/unibas/charmmtools/test/gui/CHARMM_Input_Simple.java
+++ src/ch/unibas/charmmtools/test/gui/CHARMM_Input_Simple.java
@@ -34,7 +34,7 @@ public class CHARMM_Input_Simple extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("CHARMM_Input_Simple_fxml.fxml"));
 
-        Scene scene = new Scene(root, 300, 275);
+        Scene scene = new Scene(root, 1024, 768);
 
         stage.setTitle("FXML Welcome");
         stage.setScene(scene);
