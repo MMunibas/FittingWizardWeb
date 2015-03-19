961202a95cefb582778764a99544cbad671444dd
FHedin <hedin.florent@gmail.com>
2015-03-19 15:15:49 +0100

    still buggy fullgrid

--- src/ch/unibas/charmmtools/gui/step5_grid/CHARMM_GUI_Step5_grid.java
+++ src/ch/unibas/charmmtools/gui/step5_grid/CHARMM_GUI_Step5_grid.java
@@ -9,7 +9,6 @@
 package ch.unibas.charmmtools.gui.step5_grid;
 
 import ch.unibas.fittingwizard.presentation.base.WizardPage;
-import ch.unibas.fittingwizard.presentation.base.ui.EditingCell;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.beans.property.SimpleStringProperty;
@@ -20,14 +19,12 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellEditEvent;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.util.Callback;
 
 /**
  *
@@ -43,13 +40,19 @@ public class CHARMM_GUI_Step5_grid extends WizardPage {
     private Button goButton, genGrid;
 
     @FXML
-    private TableView<gridValuesModel> tableview_gridValues, tableview_fullgrid;
+    private TableView<gridValuesModel> tableview_gridValues;
+    @FXML
+    private TableView<fullGridModel> tableview_fullgrid;
+
     @FXML
     private TableColumn<gridValuesModel, String> column_gridValues;
-    @FXML
-    private List<TableColumn<gridValuesModel, String>> columns_fullgrid;
-
     private ObservableList<gridValuesModel> list_gridValues;
+    private ObservableList<fullGridModel> list_fullgrid;
+
+    private TableColumn<fullGridModel, String> epsCol;
+    @FXML
+    private List<TableColumn<fullGridModel, String>> columns_fullgrid;
+//    private List<ObservableList<fullGridModel>> list_fullGrid;
 
 //    private Callback<TableColumn<gridValuesModel,String>,TableCell<gridValuesModel,String>> cellFactory;
     /**
@@ -71,12 +74,55 @@ public class CHARMM_GUI_Step5_grid extends WizardPage {
         }
 
         /**
-         * @param value the value to set
+         * @param val the value to set
          */
         public void setValue(String val) {
             value.set(val);
         }
 
+    }
+
+    public class fullGridModel {
+
+        private StringProperty epsilon;
+//        private List<StringProperty> valList = new ArrayList<>();
+//        
+//        private fullGridModel(String epsi, String... val)
+//        {
+//            this.epsilon = new SimpleStringProperty(epsi);
+//            for(String st : val)
+//                valList.add(new SimpleStringProperty(st));
+//        }
+//        
+//        private fullGridModel(String epsi, List<String> val)
+//        {
+//            this.epsilon = new SimpleStringProperty(epsi);
+//            for(String st : val)
+//                valList.add(new SimpleStringProperty(st));
+//        }
+        private StringProperty data;
+
+        public fullGridModel(String epsi, String dat) {
+            this.epsilon = new SimpleStringProperty(epsi);
+            this.data = new SimpleStringProperty(dat);
+        }
+
+        public void setEps(String eps){
+            this.epsilon.setValue(eps);
+        }
+        
+        public void setDat(String dat){
+            this.data.setValue(dat);
+        }
+        
+        public String getEps(){
+            return this.epsilon.get();
+        }
+        
+        public String getDat(){
+            return this.data.get();
+        }
+        
     }
 
     public CHARMM_GUI_Step5_grid() {
@@ -140,12 +186,34 @@ public class CHARMM_GUI_Step5_grid extends WizardPage {
     }
 
     private void setupFullGrid() {
-        tableview_fullgrid.getColumns().clear();
 
         int ngrid = Integer.valueOf(textfield_ngrid.getText());
-        
-        columns_fullgrid.add(new TableColumn("epsilons"));
 
+        tableview_fullgrid.getColumns().clear();
+
+//        list_fullGrid = new ArrayList<>();
+//        for( ObservableList<fullGridModel> obl : list_fullGrid)
+//        {
+//            
+//        }
+//        columns_fullgrid.add(new TableColumn("epsilons"));
+
+        epsCol = new TableColumn("Epsilons");
+        epsCol.setSortable(false);
+        epsCol.setCellValueFactory(
+                    new PropertyValueFactory<fullGridModel, String>("epsilon")
+            );
+//        epsCol.setCellFactory(TextFieldTableCell.forTableColumn());
+//        epsCol.setOnEditCommit(
+//                    new EventHandler<CellEditEvent<fullGridModel, String>>() {
+//                        @Override
+//                        public void handle(CellEditEvent<fullGridModel, String> t) {
+//                            ((fullGridModel) t.getTableView().getItems().get(
+//                                    t.getTablePosition().getRow())).setEps(t.getNewValue());
+//                        }
+//                    }
+//            );
+        
         for (int i = 0; i < ngrid; i++) {
             String scale = list_gridValues.get(i).getValue();
             columns_fullgrid.add(new TableColumn("sigma*" + scale));
@@ -153,24 +221,33 @@ public class CHARMM_GUI_Step5_grid extends WizardPage {
 
         for (TableColumn col : columns_fullgrid) {
             col.setSortable(false);
-            col.setPrefWidth(tableview_fullgrid.getWidth() / ((double) ngrid+1));
-            col.setCellValueFactory(
-                    new PropertyValueFactory<gridValuesModel, String>("value")
-            );
-            col.setCellFactory(TextFieldTableCell.forTableColumn());
-            col.setOnEditCommit(
-                    new EventHandler<CellEditEvent<gridValuesModel, String>>() {
-                        @Override
-                        public void handle(CellEditEvent<gridValuesModel, String> t) {
-                            ((gridValuesModel) t.getTableView().getItems().get(
-                                    t.getTablePosition().getRow())).setValue(t.getNewValue());
-                        }
-                    }
-            );
+            col.setPrefWidth(tableview_fullgrid.getWidth() / ((double) ngrid + 1));
+//            col.setCellValueFactory(
+//                    new PropertyValueFactory<fullGridModel, String>("data")
+//            );
+//            col.setCellFactory(TextFieldTableCell.forTableColumn());
+//            col.setOnEditCommit(
+//                    new EventHandler<CellEditEvent<fullGridModel, String>>() {
+//                        @Override
+//                        public void handle(CellEditEvent<fullGridModel, String> t) {
+//                            ((fullGridModel) t.getTableView().getItems().get(
+//                                    t.getTablePosition().getRow())).setDat(t.getNewValue());
+//                        }
+//                    }
+//            );
         }
 
-        tableview_fullgrid.getColumns().addAll(columns_fullgrid);
-
+        tableview_fullgrid.getColumns().add(epsCol);
+//        tableview_fullgrid.getColumns().addAll(columns_fullgrid);
+        
+        list_fullgrid = FXCollections.observableArrayList();
+        for (int i = 0; i < ngrid; i++) {
+            String scale = list_gridValues.get(i).getValue();
+            list_fullgrid.add(new fullGridModel("epsilon*"+scale, "0.0"));
+        }
+        
+        
+        tableview_fullgrid.setItems(list_fullgrid);
     }
 
     @FXML
