9923e2a06f8e4eb4a9e08090f4574dfda9ba29ac
mhelmer@mobile <martin.helmer@scs.ch>
2016-06-17 09:51:38 +0200

    ! web: added dummy fitting pages

---
+++ fitting/fitting-web/src/main/java/ch/unibas/fitting/web/gaussian/fit/step1/DialogComponent.java
@@ -0,0 +1,9 @@
+package ch.unibas.fitting.web.gaussian.fit.step1;
+
+import org.apache.wicket.Component;
+
+/**
+ * Created by mhelmer-mobile on 17.06.2016.
+ */
+public class DialogComponent {
+}
---
+++ fitting/fitting-web/src/main/java/ch/unibas/fitting/web/gaussian/fit/step2/FittingResultsPage.java
@@ -0,0 +1,9 @@
+package ch.unibas.fitting.web.gaussian.fit.step2;
+
+import ch.unibas.fitting.web.web.HeaderPage;
+
+/**
+ * Created by mhelmer-mobile on 17.06.2016.
+ */
+public class FittingResultsPage extends HeaderPage {
+}
