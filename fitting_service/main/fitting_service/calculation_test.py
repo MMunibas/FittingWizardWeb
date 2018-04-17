701962bdd09e4a8a0baece142671717f41595859
Emanuel Knecht <emanuel.knecht@scs.ch>
2018-04-17 15:43:56 +0200

    Fix: wrong relative path leads to hidden data directory

--- fitting_service/main/fitting_service/calculation_test.py
+++ fitting_service/main/fitting_service/calculation_test.py
@@ -17,7 +17,7 @@ class CalculationTest(object):
         self.run_dir = None
         self.logger = logging.getLogger(self.__class__.__name__)
         Storage().initialize()
-        Storage().set_root("..\\data")
+        Storage().set_root("../data")
 
     def execute(self):
         self._validate_input()
