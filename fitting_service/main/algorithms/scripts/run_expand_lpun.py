6615d0b9acc57eccb9e85797537cbc9ccd0d210b
Web Fitting Environment <wfit@pc-beethoven.cluster>
2019-02-25 17:34:31 +0100

    Mike: correction to lpun expansion

--- fitting_service/main/algorithms/scripts/run_expand_lpun.py
+++ fitting_service/main/algorithms/scripts/run_expand_lpun.py
@@ -3,9 +3,10 @@ from subprocess import call
 
 def run_expand_lpun(lpun, nmol, inpdir):
 
-  pureliq_lpun=inpdir + "pureliquid.lpun"
+  pureliq_lpun_name="pureliquid.lpun"
+  pureliq_lpun=inpdir + pureliq_lpun_name
   scriptdir=os.path.dirname(os.path.realpath(__file__))
   script=scriptdir+"/expand-lpun-for-identical-mols.sh"
   call([script,lpun,nmol,pureliq_lpun])
 
-  return pureliq_lpun
+  return pureliq_lpun_name
